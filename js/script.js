47cc76ace8ac250c4477331c27b87b3b4d9b9932
KC Paul <kathclaire.paul@gmail.com>
2023-02-25 10:30:01 -0800

    Week 4

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
-$(function() {
-    $(".carousel").carousel( { interval: 2000 } );
-    $("#carouselButton").click(function(){
+$(function () {
+    $(".carousel").carousel({ interval: 2000 });
+    $("#carouselButton").click(function () {
         if ($("#carouselButton").children("i").hasClass("fa-pause")) {
             $(".carousel").carousel("pause");
             $("#carouselButton").children("i").removeClass("fa-pause");
@@ -8,7 +8,19 @@ $(function() {
         } else {
             $(".carousel").carousel("cycle");
             $("#carouselButton").children("i").removeClass("fa-play");
-            $("#carouselButton").children("i").addClass("fa-pause"); 
+            $("#carouselButton").children("i").addClass("fa-pause");
+        }
+    });
+});
+
+$(function () {
+    $('#reserveButton, #loginButton').click(function () {
+        let buttonId = $(this).attr('id'); // gets the ID of the clicked button
+        if (buttonId === 'reserveButton') {
+            $('#reserveModal').modal('show');
+        }
+        else if (buttonId === 'loginButton') {
+            $('#loginModal').modal('show');
         }
     });
 });
